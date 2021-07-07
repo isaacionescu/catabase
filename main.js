1d78127d895f25b17232d4a603d11145b2829495
isaacionescu <isaac.ionescu@gmail.com>
2021-07-07 01:16:08 +0100

    v. 1.13 // dynamic API tag filtering (buggy)
    • previously up until v. 1.12 I was under the spell of a brainfart and thought that the tag filtering query provided by the API wasn't "exclusive" as opposed to "inclusive", and so that's why I had developed my own architecture of painstakingly fetching the whole raw data, then categorizing everything locally, filtering and distilling it, and finally using the .id to individually add photos to the grid and so on and so on;
    • I then realized it's the same thing if the API or I handle the filter by tags process, so I refactored everything and now I'm dynamically interpolating a string with all live current selected tags in order to call the API query;
    • still, I have bugs related to clearing the slate and to the order of execution, which must be logic errors based on async hierarchy; need to be fixed later;

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 // main site:                    https://cataas.com
 // JSON collection of all cats:  https://cataas.com/api/cats
 // JSON collection w/ filtering: https://cataas.com/api/cats?tags=cute
+// example of filtered tags:     https://cataas.com/api/cats?tags=tag1,tag2&skip=0&limit=10
 // example of an individual cat: https://cataas.com/cat/595f280e557291a9750ebf9f
 
 const body = document.querySelector('body');
 const catsGrid = document.querySelector('.cats-grid');
 const categoriesForm = document.querySelector('.categories-form');
 const ul = document.querySelector('ul');
+const baseUrl = 'https://cataas.com';
 const apiUrl = 'https://cataas.com/api/cats';
 const apiRandom = 'https://cataas.com/cat';
 
 let dynamicTagsArray = [];
-let fullCatData = [];
-const maxCats = 5
-/*console.log(`allRawCatData:`)
-console.log(allRawCatData)*/
+const maxCats = 10
 
 
 async function getCats() {
@@ -23,13 +22,11 @@ async function getCats() {
     return catsArray;
 };
 
+
 function fillCategoriesForm() {
 	getCats()
 		.then((rawCatData) => {
 			let myTagsArray = [];
-			fullCatData = rawCatData;
-			// console.log(`allRawCatData below:`)
-			// console.log(fullCatData)
 
 			function createNewTagsArray() {
 				for (let i = 0; i <= maxCats; i++) {
@@ -49,7 +46,6 @@ function fillCategoriesForm() {
 			function createCheckboxesForEachTag() {
 				myTagsArray.forEach(element => {
 					const newCheckBox = document.createElement('input');
-					// newCheckBox.innerHTML = `class="tag-item" type="checkbox" name="${element}" id="${element}"`
 					newCheckBox.classList.add('tag-item');
 					newCheckBox.type = "checkbox";
 					newCheckBox.name = element;
@@ -81,81 +77,65 @@ function filterResultsByTag() {
 			if (event.target.checked) {
 				console.log(`✅ Selected: ${tag}`)
 				dynamicTagsArray.push(tag)
-				preFilter(tag)
 			}
 			else if (!event.target.checked) {
 				console.log(`❌ Deselected: ${tag}`)
 				dynamicTagsArray = dynamicTagsArray.filter(element => (element != tag));
-				filterRemove(tag)
 			}
 		console.log(`// Current dynamicTagsArray: [${dynamicTagsArray}]`)
+		filterCats()
 		}
 	})
 }
 
-function filterRemove(tag) {
-	let allCatsOnDOM = document.getElementsByClassName(tag);
-	if(allCatsOnDOM.length > 0) {
-		console.log(allCatsOnDOM.length)
-		for (let i = 0; i < allCatsOnDOM.length; i++) {
-			const individualCat = document.querySelector(`.${tag}`)
-			individualCat.parentNode.removeChild(individualCat)
-		}
+
+function filterCats() {
+	// console.log(`// Current dynamicTagsArray: [${dynamicTagsArray}]`)
+	const allCatItems = document.getElementsByClassName('cat-item')
+	console.log(allCatItems)
+
+	let string = dynamicTagsArray.join(',');
+	async function getFilteredCats() {
+		let response = await fetch(`${apiUrl}?tags=${string}&skip=0&limit=${maxCats}`);
+		let promise = await response.json();
+		return promise;
 	}
-}
 
+	getFilteredCats()
+		.then(catData => {
+		console.log(`// Current dynamicTagsArray: [${dynamicTagsArray}]`)
 
-function preFilter(tagID) {
-	dynamicTagsArray.forEach(myTag => {
-		for(let i = 0; i < dynamicTagsArray.length; i++) {
-			fullCatData[i].tags.forEach(element => {
-				// console.log(tagID)
-				if(myTag === element) {
-					console.log(`MATCH!`)
-					filterAdd(fullCatData[i])
-				}
-			})
-		}
-	}) 
-}
+			for (let cat of allCatItems) {
+				console.log(cat)
+				cat.parentNode.removeChild(cat)
+			}
 
+			console.log(catData)
 
-function filterAdd(catItem) {
-	// console.log(catItem)
-	getCats()
-		.then((data) => {
-			let newCat = document.createElement('div');
-			newCat.classList.add('cat-item');
-			newCat.id = `abc-${catItem.id}`;
+			for (let i = 0; i < catData.length; i++) {
+				let newCat = document.createElement('div');
+				newCat.classList.add('cat-item');
 
-			catItem.tags.forEach(tag => {
-				newCat.classList.add(`${tag}`)
-			})
-			console.log(newCat.classList)
+				const catImage = document.createElement('div');
+				catImage.classList.add('cat-image');
+				const catTitle = document.createElement('div');
+				catTitle.classList.add('cat-title');
+				const catText = document.createElement('div');
+				catText.classList.add('cat-text');
 
-			const catImage = document.createElement('div');
-			catImage.classList.add('cat-image');
-			const catTitle = document.createElement('div');
-			catTitle.classList.add('cat-title');
-			const catText = document.createElement('div');
-			catText.classList.add('cat-text');
+				catImage.style.background = `url(https://cataas.com/cat/${catData[i].id}) 30% 40%`;
+				catTitle.innerText = `Cat #${i + 1}`
+				catText.innerHTML = `Tags: <br>${catData[i].tags}`
 
-			catImage.style.background = `url(https://cataas.com/cat/${catItem.id}) 30% 40%`;
-			// catTitle.innerText = `Cat #${i + 1}`
-			catText.innerHTML = `Tags: <br>${catItem.tags}`
-
-			newCat.appendChild(catImage); 
-			newCat.appendChild(catTitle); 
-			newCat.appendChild(catText);
-			catsGrid.appendChild(newCat);
-		}
-	)
+				newCat.appendChild(catImage); 
+				newCat.appendChild(catTitle); 
+				newCat.appendChild(catText);
+				catsGrid.appendChild(newCat);
+			}
+		})
+	
 		.catch(error => console.error(error))
 }
-
-
-
-
 
 
 
@@ -199,7 +179,5 @@ function fillCatsGrid() {
 	)
 	.catch(error => console.error(error))
 }
-
-// fillCatsGrid()
 
 // console.log('Test log')
