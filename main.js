72fe9bab89f04c67a076f196b856673e81698ce0
isaacionescu <isaac.ionescu@gmail.com>
2021-08-03 22:08:11 +0100

    v. 1.29.81

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ async function doWork() {
 doWork();
 
 function checkScreenSize() {
-	let mediaQuery = window.matchMedia('(max-width:719px)');
+	let mediaQuery = window.matchMedia('(max-width:899px)');
 	function onScreenSizeChange(event) {
 		if(event.matches) {
 			// console.log(`It's mobile`);
